afbee088f24ea6d2c97bcd17e281d2841aacf74b
Andrey Serikov <as.korag@gmail.com>
2021-12-02 23:06:26 +0300

    feat(api): add post methods
    Add following methods: add user, get products by list, change wishlist, change purchase

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,6 +7,18 @@ export default class API {
     return serializedResponse.data;
   }
 
+  async #post(path: string, data: any) {
+    const response = await fetch(`${this.#baseURL}${path}`, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const serializedResponse = await response.json();
+    return serializedResponse.data;
+  }
+
   getAllUsers() {
     return this.#get('/users');
   }
@@ -21,5 +33,28 @@ export default class API {
 
   getProductsByFilter(filter: string) {
     return this.#get(`/product/filter?filter=${filter}`);
+  }
+
+  addUser(name: string) {
+    return this.#post('/user', { name });
+  }
+
+  getProductsByList(listProductsID: string[]) {
+    return this.#post('/products', { listProductsId: listProductsID });
+  }
+
+  changeWishlist(userID: string, productID: string) {
+    return this.#post('/user/wishlist', {
+      userId: userID,
+      productId: productID,
+    });
+  }
+
+  changePurchase(userID: string, productID: string, isAdd: boolean) {
+    return this.#post('/user/purchase', {
+      userId: userID,
+      productId: productID,
+      isAdd,
+    });
   }
 }
