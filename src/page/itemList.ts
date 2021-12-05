aa19707a62f6f6b926a88ad10ce03dbc7cccd243
Zahar Bystrok <zbystrok.jr@gmail.com>
2021-12-05 17:04:31 +0300

    feat(ts): add item.ts itemList.ts shopping.ts dataMethods.ts

---
+++ src/page/itemList.ts
@@ -0,0 +1,210 @@
+interface Product {
+  productDescription: {
+    span: string;
+    name: string;
+    type: string;
+    country: string;
+    description: string;
+  };
+  productImage: string;
+  productCost: string;
+  productID: number;
+}
+
+const productData: Product[] = [
+  {
+    productDescription: {
+      span: '1',
+      name: 'ИС-64',
+      type: '',
+      country: '',
+      description:
+        'Premium Account allows to research desirable vehicles quicker and to save time on its purchase. It gives more Credits and Experience per battle.  It is a great deal as the profit after paying for repairs and ammo increases two- or threefold, depending on your performance in battle (amount of damage, destroyed enemies, base captured/defended, enemies detected, etc).',
+    },
+    productImage:
+      'https://ru-wotp.wgcdn.co/dcont/fb/image/r165_object_703_ii_1300_big_1.png',
+    productCost: '$ 58.99',
+    productID: 1,
+  },
+  {
+    productDescription: {
+      span: '2',
+      name: 'ИС-6',
+      type: '',
+      country: '',
+      description:
+        'Premium Account allows to research desirable vehicles quicker and to save time on its purchase. It gives more Credits and Experience per battle.  It is a great deal as the profit after paying for repairs and ammo increases two- or threefold, depending on your performance in battle (amount of damage, destroyed enemies, base captured/defended, enemies detected, etc).',
+    },
+    productImage:
+      'https://ru-wotp.wgcdn.co/dcont/fb/image/r165_object_703_ii_1300_big_1.png',
+    productCost: '$ 58.99',
+    productID: 2,
+  },
+  {
+    productDescription: {
+      span: '2',
+      name: 'ИС-6',
+      type: '',
+      country: '',
+      description:
+        'Premium Account allows to research desirable vehicles quicker and to save time on its purchase. It gives more Credits and Experience per battle.  It is a great deal as the profit after paying for repairs and ammo increases two- or threefold, depending on your performance in battle (amount of damage, destroyed enemies, base captured/defended, ' +
+        'enemies detected, etc).',
+    },
+    productImage:
+      'https://ru-wotp.wgcdn.co/dcont/fb/image/r165_object_703_ii_1300_big_1.png',
+    productCost: '$ 58.99',
+    productID: 3,
+  },
+  {
+    productDescription: {
+      span: '2',
+      name: 'ИС-6',
+      type: '',
+      country: '',
+      description:
+        'Premium Account allows to research desirable vehicles quicker and to save time on its purchase. It gives more Credits and Experience per battle.  It is a great deal as the profit after paying for repairs and ammo increases two- or threefold, depending on your performance in battle (amount of damage, destroyed enemies, base captured/defended, ' +
+        'enemies detected, etc).',
+    },
+    productImage:
+      'https://ru-wotp.wgcdn.co/dcont/fb/image/r165_object_703_ii_1300_big_1.png',
+    productCost: '$ 58.99',
+    productID: 4,
+  },
+  {
+    productDescription: {
+      span: '2',
+      name: 'ИС-6',
+      type: '',
+      country: '',
+      description:
+        'Premium Account allows to research desirable vehicles quicker and to save time on its purchase. It gives more Credits and Experience per battle.  It is a great deal as the profit after paying for repairs and ammo increases two- or threefold, depending on your performance in battle (amount of damage, destroyed enemies, base captured/defended, ' +
+        'enemies detected, etc).',
+    },
+    productImage:
+      'https://ru-wotp.wgcdn.co/dcont/fb/image/r165_object_703_ii_1300_big_1.png',
+    productCost: '$ 58.99',
+    productID: 5,
+  },
+  {
+    productDescription: {
+      span: '1',
+      name: 'ИС-6',
+      type: '',
+      country: '',
+      description:
+        'Premium Account allows to research desirable vehicles quicker and to save time on its purchase. It gives more Credits and Experience per battle.  It is a great deal as the profit after paying for repairs and ammo increases two- or threefold, depending on your performance in battle (amount of damage, destroyed enemies, base captured/defended, ' +
+        'enemies detected, etc).',
+    },
+    productImage:
+      'https://ru-wotp.wgcdn.co/dcont/fb/image/r165_object_703_ii_1300_big_1.png',
+    productCost: '$ 58.99',
+    productID: 6,
+  },
+  {
+    productDescription: {
+      span: '1',
+      name: 'ИС-6',
+      type: '',
+      country: '',
+      description:
+        'Premium Account allows to research desirable vehicles quicker and to save time on its purchase. It gives more Credits and Experience per battle.  It is a great deal as the profit after paying for repairs and ammo increases two- or threefold, depending on your performance in battle (amount of damage, destroyed enemies, base captured/defended, ' +
+        'enemies detected, etc).',
+    },
+    productImage:
+      'https://ru-wotp.wgcdn.co/dcont/fb/image/r165_object_703_ii_1300_big_1.png',
+    productCost: '$ 58.99',
+    productID: 7,
+  },
+  {
+    productDescription: {
+      span: '1',
+      name: 'ИС-6',
+      type: '',
+      country: '',
+      description:
+        'Premium Account allows to research desirable vehicles quicker and to save time on its purchase. It gives more Credits and Experience per battle.  It is a great deal as the profit after paying for repairs and ammo increases two- or threefold, depending on your performance in battle (amount of damage, destroyed enemies, base captured/defended, ' +
+        'enemies detected, etc).',
+    },
+    productImage:
+      'https://ru-wotp.wgcdn.co/dcont/fb/image/r165_object_703_ii_1300_big_1.png',
+    productCost: '$ 58.99',
+    productID: 8,
+  },
+  {
+    productDescription: {
+      span: '1',
+      name: 'ИС-6',
+      type: '',
+      country: '',
+      description:
+        'Premium Account allows to research desirable vehicles quicker and to save time on its purchase. It gives more Credits and Experience per battle.  It is a great deal as the profit after paying for repairs and ammo increases two- or threefold, depending on your performance in battle (amount of damage, destroyed enemies, base captured/defended, ' +
+        'enemies detected, etc).',
+    },
+    productImage:
+      'https://ru-wotp.wgcdn.co/dcont/fb/image/r165_object_703_ii_1300_big_1.png',
+    productCost: '$ 58.99',
+    productID: 9,
+  },
+  {
+    productDescription: {
+      span: '1',
+      name: 'ИС-6',
+      type: '',
+      country: '',
+      description:
+        'Premium Account allows to research desirable vehicles quicker and to save time on its purchase. It gives more Credits and Experience per battle.  It is a great deal as the profit after paying for repairs and ammo increases two- or threefold, depending on your performance in battle (amount of damage, destroyed enemies, base captured/defended, ' +
+        'enemies detected, etc).',
+    },
+    productImage:
+      'https://ru-wotp.wgcdn.co/dcont/fb/image/r165_object_703_ii_1300_big_1.png',
+    productCost: '$ 58.99',
+    productID: 10,
+  },
+  {
+    productDescription: {
+      span: '1',
+      name: 'ИС-6',
+      type: '',
+      country: '',
+      description:
+        'Premium Account allows to research desirable vehicles quicker and to save time on its purchase. It gives more Credits and Experience per battle.  It is a great deal as the profit after paying for repairs and ammo increases two- or threefold, depending on your performance in battle (amount of damage, destroyed enemies, base captured/defended, ' +
+        'enemies detected, etc).',
+    },
+    productImage:
+      'https://ru-wotp.wgcdn.co/dcont/fb/image/r165_object_703_ii_1300_big_1.png',
+    productCost: '$ 58.99',
+    productID: 11,
+  },
+  {
+    productDescription: {
+      span: '1',
+      name: 'ИС-6',
+      type: '',
+      country: '',
+      description:
+        'Premium Account allows to research desirable vehicles quicker and to save time on its purchase. It gives more Credits and Experience per battle.  It is a great deal as the profit after paying for repairs and ammo increases two- or threefold, depending on your performance in battle (amount of damage, destroyed enemies, base captured/defended, ' +
+        'enemies detected, etc).',
+    },
+    productImage:
+      'https://ru-wotp.wgcdn.co/dcont/fb/image/r165_object_703_ii_1300_big_1.png',
+    productCost: '$ 58.99',
+    productID: 12,
+  },
+  {
+    productDescription: {
+      span: '1',
+      name: 'ИС-6',
+      type: '',
+      country: '',
+      description:
+        'Premium Account allows to research desirable vehicles quicker and to save time on its purchase. It gives more Credits and Experience per battle.  It is a great deal as the profit after paying for repairs and ammo increases two- or threefold, depending on your performance in battle (amount of damage, destroyed enemies, base captured/defended, ' +
+        'enemies detected, etc).',
+    },
+    productImage:
+      'https://ru-wotp.wgcdn.co/dcont/fb/image/r165_object_703_ii_1300_big_1.png',
+    productCost: '$ 58.99',
+    productID: 13,
+  },
+];
+
+export { Product, productData };
