98baf5ab130be532b2afa5adcf7c29182627b757
Andrey Serikov <as.korag@gmail.com>
2021-12-06 23:53:00 +0300

    refactor(api): change and split api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,91 +1,10 @@
-import { IUser } from '@type/user';
-import { IProduct } from '@type/product';
-import {
-  IAddUserReq,
-  IGetProductsByListReq,
-  IChangeWishlistReq,
-  IChangePurchaseReq,
-} from '@type/api';
+import axios, { AxiosRequestConfig } from 'axios';
 
-export default class API {
-  readonly #baseURL = 'https://wg-force3-backend.herokuapp.com/api';
+const baseURL = `https://wg-force3-backend.herokuapp.com/api/`;
 
-  async #get(path: string) {
-    const response = await fetch(`${this.#baseURL}${path}`);
-    if (response.statusText === 'OK') {
-      const serializedResponse = await response.json();
-      return serializedResponse.data;
-    }
-    throw new Error('Ooops!');
-  }
+const instance = axios.create({
+  baseURL,
+  withCredentials: false,
+} as AxiosRequestConfig);
 
-  async #post(
-    path: string,
-    data:
-      | IAddUserReq
-      | IGetProductsByListReq
-      | IChangeWishlistReq
-      | IChangePurchaseReq,
-  ) {
-    const response = await fetch(`${this.#baseURL}${path}`, {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const serializedResponse = await response.json();
-    return serializedResponse.data;
-  }
-
-  getAllUsers() {
-    const result: Promise<IUser[]> = this.#get('/users');
-    return result;
-  }
-
-  getUserByID(id: string) {
-    const result: Promise<IUser> = this.#get(`/user?id=${id}`);
-    return result;
-  }
-
-  getProductByID(id: string) {
-    const result: Promise<IProduct> = this.#get(`/product?id=${id}`);
-    return result;
-  }
-
-  getProductsByFilter(filter: string) {
-    const result: Promise<IProduct[]> = this.#get(
-      `/product/filter?filter=${filter}`,
-    );
-    return result;
-  }
-
-  addUser(name: string) {
-    const result: Promise<IUser> = this.#post('/user', { name });
-    return result;
-  }
-
-  getProductsByList(listProductsID: string[]) {
-    const result: Promise<IProduct[]> = this.#post('/products', {
-      listProductsId: listProductsID,
-    });
-    return result;
-  }
-
-  changeWishlist(userID: string, productID: string) {
-    const result: Promise<IUser> = this.#post('/user/wishlist', {
-      userId: userID,
-      productId: productID,
-    });
-    return result;
-  }
-
-  changePurchase(userID: string, productID: string, isAdd: boolean) {
-    const result: Promise<IUser> = this.#post('/user/purchase', {
-      userId: userID,
-      productId: productID,
-      isAdd,
-    });
-    return result;
-  }
-}
+export default instance;
---
+++ src/api/product.ts
@@ -0,0 +1,27 @@
+import instance from '@api/api';
+import { IProduct, TFilter } from '@type/product';
+import { IResponse } from '@type/api';
+
+class ProductAPI {
+  static getProductByID(id: string) {
+    return instance
+      .get<IResponse<IProduct>>(`product?id=${id}`)
+      .then((response) => response.data);
+  }
+
+  static getProductsByFilter(filter: TFilter) {
+    return instance
+      .get<IResponse<IProduct[]>>(`product/filter?filter=${filter}`)
+      .then((response) => response.data);
+  }
+
+  static getProductsByList(listProductsID: string[]) {
+    return instance
+      .post<IResponse<IProduct[]>>('products', {
+        listProductsId: listProductsID,
+      })
+      .then((response) => response.data);
+  }
+}
+
+export default ProductAPI;
