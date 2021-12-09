e6e9bd2f6158edc57de7f9a535dd11512b0de01f
Zahar Bystrok <zbystrok.jr@gmail.com>
2021-12-09 17:49:59 +0300

    fix(shopping): rename, add changeWishlist and showWishlist methods

---
+++ src/scripts/changeUserLists.ts
@@ -0,0 +1,56 @@
+import { IProduct } from '@type/product';
+import LocalStorage from '@scripts/localStorage';
+
+class ChangeUserLists {
+  // show number of shopping list
+  static showShoppingList(shopping: string[]): void {
+    const $shoppingCounter: HTMLElement | null = document.querySelector(
+      '.cart-span-container',
+    );
+    if ($shoppingCounter) {
+      $shoppingCounter.textContent = `(${shopping.length})`;
+    }
+  }
+
+  // change product in shoppingList
+  static changeShoppingList(
+    product: IProduct,
+    shopping: string[],
+    showShopping: (shopping: string[]) => void,
+  ): void {
+    shopping.push(product.data.id);
+    LocalStorage.changeLocalShoppingList('user', product.data.id);
+    showShopping(shopping);
+  }
+
+  // show number of wishlist
+  static showWishlist(wishList: string[]): void {
+    const $wishListCounter: HTMLElement | null = document.querySelector(
+      '.wishlist-span-container',
+    );
+    if ($wishListCounter) {
+      $wishListCounter.textContent = `(${wishList.length})`;
+    }
+  }
+
+  // change product in wishlist
+  static changeWishlist(
+    product: IProduct,
+    wishlist: string[],
+    showWishList: (wishlist: string[]) => void,
+    $element: HTMLElement,
+  ): void {
+    const index: number = wishlist.indexOf(product.data.id);
+    if (index !== -1) {
+      wishlist.splice(index, 1);
+    } else {
+      wishlist.push(product.data.id);
+    }
+    LocalStorage.changeLocalWishlist('user', wishlist);
+    showWishList(wishlist);
+    // add or remove style for like button
+    $element.classList.toggle('button-like_active'); //! disable button!
+  }
+}
+
+export default ChangeUserLists;
