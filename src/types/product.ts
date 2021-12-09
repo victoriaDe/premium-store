f77d3c52fa2ea39f584e66386ae29994e1fcdca3
Andrey Serikov <as.korag@gmail.com>
2021-12-10 00:06:42 +0300

    docs(types): add some docs comments for types

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,3 +1,7 @@
+/**
+ * @module Product Types
+ */
+
 type TCountry =
   | 'china'
   | 'czech'
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,6 +1,14 @@
+/**
+ * @module User Type
+ */
+
 export interface IUser {
+  /** user ID */
   id: string;
+  /** username */
   name: string;
+  /** array of product IDs in the wishlist */
   wishlist: Array<string>;
+  /** array of product IDs in the cart */
   shoppingList: Array<string>;
 }
