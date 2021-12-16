74d14e73414aa685a20ceba540c0b031494797e1
Andrey Serikov <as.korag@gmail.com>
2021-12-17 02:19:23 +0300

    feat(none): create MainPage class in a separate file

--- src/page/main.ts
+++ src/page/main.ts
@@ -1,17 +1,10 @@
-import { IUser } from '@type/user';
-import { IProduct, TFilter } from '@type/product';
-
-import UserAPI from '@api/user';
-import Item from '@classes/Item';
-import ProductAPI from '@api/product';
+import MainPage from '@classes/MainPage';
 import Filter from '@scripts/filter';
 import Wishlist from '@classes/Wishlist';
-import LocalStorage from '@classes/LocalStorage';
 import ShoppingList from '@classes/ShoppingList';
 import Popup from '@classes/Popup';
 import Router from '@classes/Router';
 import Navigation from '@classes/Navigation';
-import lazy from '@scripts/lazy';
 
 import '@scss/main.scss';
 import '@scss/variables/colors.scss';
@@ -52,6 +45,8 @@ router
 
 router.init();
 
+export { router };
+
 const $wishlistLink = document.getElementById('wishlistId') as HTMLElement;
 
 $wishlistLink.addEventListener('click', (event) => {
@@ -81,78 +76,6 @@ $headerLogoLink.addEventListener('click', (event) => {
 //     router.changeURI(`?filter=${$target.dataset.filter}`);
 //   });
 // });
-
-class MainPage {
-  #productData: IProduct[] | null = [];
-
-  #userData: IUser | null = null;
-
-  #userId = '61a6286353b5dad92e57b4c0';
-
-  async getAllData() {
-    const productData = await this.getProductDataByFilter('All');
-    const userData = await this.getUserData();
-    return [productData, userData];
-  }
-
-  async updateUserData() {
-    const userData = await UserAPI.getUserByID(this.#userId);
-    localStorage.setItem('user', JSON.stringify(userData));
-    return userData;
-  }
-
-  async sendUserData() {
-    const userData = LocalStorage.getLocalData('user') as IUser | null;
-    if (userData) {
-      const data = await UserAPI.changeUserData(userData);
-    }
-  }
-
-  async updateProductDataByFilter(filter: TFilter | 'All') {
-    const productDataByFilter = await ProductAPI.getProductsByFilter(filter);
-    localStorage.setItem(filter, JSON.stringify(productDataByFilter));
-    return productDataByFilter;
-  }
-
-  async getProductDataByFilter(filter: TFilter | 'All') {
-    let productDataByFilter = LocalStorage.getLocalData(filter) as
-      | IProduct[]
-      | null;
-    if (!productDataByFilter)
-      productDataByFilter = await this.updateProductDataByFilter(filter);
-    return productDataByFilter;
-  }
-
-  async getUserData() {
-    let userData = LocalStorage.getLocalData('user') as IUser | null;
-    if (!userData) userData = await this.updateUserData();
-    this.#userData = userData;
-    return userData;
-  }
-
-  async getListData(property: 'shoppingList' | 'wishlist') {
-    const userData = await this.getUserData();
-    if (userData) {
-      if (userData[property].length > 0) {
-        return ProductAPI.getProductsByList(userData[property]);
-      }
-    }
-    return null;
-  }
-
-  async init(): Promise<any> {
-    const data = (await this.getAllData()) as any[];
-    await Filter.filterProducts('all', router);
-    Filter.addEvent(router);
-    ShoppingList.showShoppingListCounter(data[1].shoppingList);
-    Wishlist.showWishlistCounter(data[1].wishlist);
-    await lazy(20, 100, data[1], data[0], new Item(), router);
-
-    setTimeout(() => {
-      this.updateUserData();
-    });
-  }
-}
 
 export const main: MainPage = new MainPage();
 
