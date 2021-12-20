1ea3b06d2d1028a201e454e58901dc096ffdef72
Andrey Serikov <AS.Korag@protonmail.com>
2021-12-20 13:46:52 +0300

    feat(none): add createEmptyPage method

--- src/classes/DOMElements.ts
+++ src/classes/DOMElements.ts
@@ -12,11 +12,23 @@ class DOMElements {
    * @param title заголовок шапки
    */
 
-  static createPartitionHeader(title: string) {
+  static createPageHeader(title: string): HTMLDivElement {
     const $header = document.createElement('div');
     $header.innerHTML = `${title}`;
     $header.classList.add('list-header-container');
     return $header;
+  }
+
+  /**
+   * Метод для создания пустых страниц корзины и списка желаний
+   * @param text выводимый на странице текст
+   */
+
+  static createEmptyPage(text: string): HTMLDivElement {
+    const $item = document.createElement('div');
+    $item.classList.add('item-filtered-container');
+    $item.innerHTML = `<div class="empty-list">${text}</div>`;
+    return $item;
   }
 }
 
