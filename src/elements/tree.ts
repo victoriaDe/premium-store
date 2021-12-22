d5ea00e2a9d9d55ea18f3199ebec88b9b5eb7406
victoriaDe <victoria.derugo@gmail.com>
2021-12-23 01:33:49 +0200

    fix(tree): animation duration 8=>4

--- src/elements/tree.ts
+++ src/elements/tree.ts
@@ -3,11 +3,5 @@ const tree: HTMLDivElement | null = document.querySelector('.tree');
 tree?.addEventListener('animationend', deleteTree);
 
 function deleteTree() {
-  // console.log(tree);
-  // console.log('----------');
   tree?.parentNode?.removeChild(tree);
-
-  // console.log(tree);
-  // console.log('----------');
-
 }
