361e128d94bc21cf48604d7e419edcffe2844d98
victoriaDe <victoria.derugo@gmail.com>
2021-12-22 13:19:25 +0200

    refactor(html): icons, import scss

--- src/page/main.ts
+++ src/page/main.ts
@@ -2,15 +2,7 @@ import '../elements/elements';
 import '../elements/tree';
 
 import '@scss/main.scss';
-import '@scss/variables/colors.scss';
-import '@scss/variables/sizes.scss';
-import '@scss/popup.scss';
-import '@scss/item.scss';
-import '@scss/items-filtered-list.scss';
-import '@scss/filters.scss';
-import '@scss/main-content.scss';
-import '@scss/item-content.scss';
-import '@scss/iconmoon.scss';
+
 
 import storeInit from '@scripts/init/store';
 import {
