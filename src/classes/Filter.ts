895465b2111adf147aa64d672f2a75a80948cda8
Zahar Bystrok <zbystrok.jr@gmail.com>
2021-12-23 12:20:37 +0300

    fix(none): change lazy and lazyBD params

--- src/classes/Filter.ts
+++ src/classes/Filter.ts
@@ -260,7 +260,7 @@ class Filter {
       });
     }
 
-    lazy(20, 100, userData, filteredProducts, new Item());
+    lazy(20, 100, userData, filteredProducts);
   }
 
   /**
@@ -308,7 +308,7 @@ class Filter {
             value.products.forEach((product: IProduct) => {
               $container.appendChild(ItemDOM.createItem(product, userData));
             });
-            lazyBD(40, 500, userData, new Item());
+            lazyBD(40, 500, userData);
           }
         },
       );
