0af556a7e3e705d3305b5319e38ea21b475505cb
Andrey Serikov <as.korag@gmail.com>
2021-12-23 09:19:08 +0300

    fix(none): api module docs

--- src/classes/api/API.ts
+++ src/classes/api/API.ts
@@ -1,18 +1,30 @@
+/**
+ * @module API
+ */
+
 import axios, { AxiosRequestConfig } from 'axios';
 
+/** URL сервера с БД */
 const backEndBaseURL = `https://wg-force3-backend.herokuapp.com/api/`;
+
+/** URL для обратного геокодирования */
 const geoLocBaseURL = 'https://eu1.locationiq.com/v1/';
+
+/** URL для определения страны по IP */
 const ipLocBaseURL = 'http://ipwhois.app/json/';
 
+/** экземпляр axios для работы с сервером и БД */
 const backEndInstance = axios.create({
   baseURL: backEndBaseURL,
   withCredentials: false,
 } as AxiosRequestConfig);
 
+/** экземпляр axios для определения местоположения пользователя по геолокации */
 const geoLocInstance = axios.create({
   baseURL: geoLocBaseURL,
 });
 
+/** экземпляр axios для определения местоположения пользователя по IP адресу */
 const ipLocInstance = axios.create({
   baseURL: ipLocBaseURL,
 });
--- src/types/api.ts
+++ src/types/api.ts
@@ -1,26 +1,37 @@
 /**
- * @module API Interfaces
+ * @module API
  */
 
 export interface IResponse<T> {
+  /** код выполнения */
   resultCode: 0 | 1;
+
+  /** ошибки (код выполнения 1) */
   messages: string[];
-  data: T | null ;
+
+  /** данные с сервера */
+  data: T | null;
 }
 
 export interface IAddUserReq {
+  /** имя пользователя */
   name: string;
 }
 
 export interface IGetProductsByListReq {
+  /** список ID продуктов */
   listProductsId: string[];
 }
 
 export interface IChangeWishlistReq {
+  /** ID пользователя */
   userId: string;
+
+  /** ID продукта */
   productId: string;
 }
 
 export interface IChangePurchaseReq extends IChangeWishlistReq {
+  /** флаг, показывающий добавлен ли продукт в корзину */
   isAdd: boolean;
 }
